79988f96010d3280cdd8475b8f85a064d7652500
Hiago Brenha <hiagobrenha@protonmail.com>
2025-01-20 21:26:59 -0300

    feat: implement category entity

---
+++ domain/src/test/java/com/codeflix/admin/catalog/domain/CategoryTest.java
@@ -0,0 +1,26 @@
+package com.codeflix.admin.catalog.domain;
+
+import com.codeflix.admin.catalog.domain.category.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CategoryTest {
+
+    @Test
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        assertNotNull(actualCategory.getId());
+        assertEquals(expectedName, actualCategory.getName());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertTrue(actualCategory.isActive());
+        assertNotNull(actualCategory.getCreatedAt());
+        assertNotNull(actualCategory.getUpdatedAt());
+        assertNull(actualCategory.getDeletedAt());
+    }
+}
