3eaac76aa49991f8e06a4a08e0b00f5d314f8d19
Hiago <hiagobrenha@protonmail.com>
2025-01-21 07:26:27 -0300

    feat: add more validations

--- domain/src/test/java/com/codeflix/admin/catalog/domain/CategoryTest.java
+++ domain/src/test/java/com/codeflix/admin/catalog/domain/CategoryTest.java
@@ -40,4 +40,88 @@ public class CategoryTest {
         assertEquals(expectedErrorCount, actualException.getErrors().size());
         assertEquals(expectedErrorMessage, actualException.getErrors().get(0).Message());
     }
+
+    @Test
+    public void givenAnInvalidEmptyName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final var expectedErrorCount = 1;
+        final var expectedEmptyName = "";
+        final var expectedErrorMessage = "'name' should not be empty";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedEmptyName, expectedDescription, expectedIsActive);
+
+        final var actualException = assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).Message());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthLessThan3_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final var expectedErrorCount = 1;
+        final var expectedEmptyName = "Na ";
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedEmptyName, expectedDescription, expectedIsActive);
+
+        final var actualException = assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).Message());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthGreaterThan255_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final var expectedErrorCount = 1;
+        final var expectedEmptyName = "N".repeat(300);
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedEmptyName, expectedDescription, expectedIsActive);
+
+        final var actualException = assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).Message());
+    }
+
+    @Test
+    public void givenAValidEmptyDescription_whenCallNewCategoryAndValidate_thenInstantiateACategory() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+        assertNotNull(actualCategory.getId());
+        assertEquals(expectedName, actualCategory.getName());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertTrue(actualCategory.isActive());
+        assertNotNull(actualCategory.getCreatedAt());
+        assertNotNull(actualCategory.getUpdatedAt());
+        assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAValidFalseIsActive_whenCallNewCategoryAndValidate_thenInstantiateACategory() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = false;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+        assertNotNull(actualCategory.getId());
+        assertEquals(expectedName, actualCategory.getName());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertFalse(actualCategory.isActive());
+        assertNotNull(actualCategory.getCreatedAt());
+        assertNotNull(actualCategory.getUpdatedAt());
+        assertNotNull(actualCategory.getDeletedAt());
+    }
 }
