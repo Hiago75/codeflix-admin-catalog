d5d1e6567023a6b729a4632fca8c330957b79743
Hiago Brenha <hiagobrenha@protonmail.com>
2025-01-20 22:11:16 -0300

    feat: add category validator

---
+++ domain/src/main/java/com/codeflix/admin/catalog/domain/category/CategoryValidator.java
@@ -0,0 +1,21 @@
+package com.codeflix.admin.catalog.domain.category;
+
+import com.codeflix.admin.catalog.domain.validation.Error;
+import com.codeflix.admin.catalog.domain.validation.ValidationHandler;
+import com.codeflix.admin.catalog.domain.validation.Validator;
+
+public class CategoryValidator extends Validator {
+    private final Category category;
+
+    public CategoryValidator(final Category aCategory, final ValidationHandler aHandler) {
+        super(aHandler);
+        this.category = aCategory;
+    }
+
+    @Override
+    public void validate() {
+        if (this.category.getName() == null || this.category.getName().isEmpty()) {
+            this.validationHandler().append(new Error("'name' should not be null"));
+        }
+    }
+}
