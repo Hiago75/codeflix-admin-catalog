c4e3192533cd1ecda813f98e83ba2cb6ccf7ef8d
Hiago <hiagobrenha@protonmail.com>
2025-01-25 18:04:58 -0300

    feat: implement spring boot

--- infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
@@ -1,9 +1,13 @@
 package com.codeflix.admin.catalog.infrastructure;
 
-import com.codeflix.admin.catalog.application.UseCase;
+import com.codeflix.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
