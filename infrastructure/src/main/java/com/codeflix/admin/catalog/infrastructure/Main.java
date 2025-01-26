78ed4a1bddae826dc1500638daf288f04b106e4e
Hiago <hiagobrenha@protonmail.com>
2025-01-26 16:41:28 -0300

    feat: implement category mysql gateway create method

--- infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
@@ -1,16 +1,9 @@
 package com.codeflix.admin.catalog.infrastructure;
 
-import com.codeflix.admin.catalog.domain.category.Category;
-import com.codeflix.admin.catalog.infrastructure.category.persistance.CategoryJpaEntity;
-import com.codeflix.admin.catalog.infrastructure.category.persistance.CategoryRepository;
 import com.codeflix.admin.catalog.infrastructure.configuration.WebServerConfig;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
-
-import java.util.List;
 
 @SpringBootApplication
 public class Main {
