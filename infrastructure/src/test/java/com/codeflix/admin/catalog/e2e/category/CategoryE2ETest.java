79815e1f106cbdc65ff0e00bd95b00b63d9eeada
Hiago <hiagobrenha@protonmail.com>
2025-02-01 11:57:53 -0300

    test: implement e2e test to create category

--- infrastructure/src/test/java/com/codeflix/admin/catalog/e2e/category/CategoryE2ETest.java
+++ infrastructure/src/test/java/com/codeflix/admin/catalog/e2e/category/CategoryE2ETest.java
@@ -1,17 +1,34 @@
 package com.codeflix.admin.catalog.e2e.category;
 
 import com.codeflix.admin.catalog.E2ETest;
-import org.junit.jupiter.api.Assertions;
+import com.codeflix.admin.catalog.domain.category.CategoryID;
+import com.codeflix.admin.catalog.infrastructure.category.models.CategoryResponse;
+import com.codeflix.admin.catalog.infrastructure.category.models.CreateCategoryRequest;
+import com.codeflix.admin.catalog.infrastructure.category.persistance.CategoryRepository;
+import com.codeflix.admin.catalog.infrastructure.configuration.json.Json;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @E2ETest
 @Testcontainers
 public class CategoryE2ETest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Container
     private static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer("mysql:8.0")
@@ -25,7 +42,51 @@ public class CategoryE2ETest {
     }
 
     @Test
-    public void testWorks() {
-        Assertions.assertTrue(MYSQL_CONTAINER.isRunning());
+    public void asACatalogAdminIShouldBeAbleToCreateANewCategoryWithValidValues() throws Exception {
+        assertTrue(MYSQL_CONTAINER.isRunning());
+
+        assertEquals(0, categoryRepository.count());
+
+        final var expectedName = "Movies";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = true;
+
+        final var actualId = givenACategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualCategory = retrieveACategory(actualId.getValue());
+
+        assertEquals(expectedName, actualCategory.name());
+        assertEquals(expectedDescription, actualCategory.description());
+        assertEquals(expectedIsActive, actualCategory.active());
+        assertNotNull(actualCategory.createdAt());
+        assertNotNull(actualCategory.updatedAt());
+        assertNull(actualCategory.deletedAt());
+    }
+
+    private CategoryID givenACategory(final String aName, final String aDescription, final boolean isActive) throws Exception {
+        final var aRequestBody = new CreateCategoryRequest(aName, aDescription, isActive);
+
+        final var aRequest = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Json.writeValueAsString(aRequestBody));
+
+        final var actualId = this.mvc.perform(aRequest)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn()
+                .getResponse().getHeader("Location")
+                .replace("/categories/", "");
+
+        return CategoryID.from(actualId);
+    }
+
+    private CategoryResponse retrieveACategory(final String anId) throws Exception {
+        final var aRequest = MockMvcRequestBuilders.get("/categories/" + anId);
+
+        final var json = this.mvc.perform(aRequest)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn()
+                .getResponse().getContentAsString();
+
+        return Json.readValue(json, CategoryResponse.class);
     }
 }
