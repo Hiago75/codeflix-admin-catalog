bf4ba93717bac5a763dc1bbe2e71bee2fef200d0
Hiago Brenha <hiagobrenha@protonmail.com>
2025-02-06 20:27:46 -0300

    feat: implement delete genre usecase

---
+++ application/src/main/java/com/codeflix/admin/catalog/application/genre/delete/DeleteGenreUseCase.java
@@ -0,0 +1,7 @@
+package com.codeflix.admin.catalog.application.genre.delete;
+
+import com.codeflix.admin.catalog.application.UnitUseCase;
+
+public abstract class DeleteGenreUseCase extends UnitUseCase<String> {
+
+}
