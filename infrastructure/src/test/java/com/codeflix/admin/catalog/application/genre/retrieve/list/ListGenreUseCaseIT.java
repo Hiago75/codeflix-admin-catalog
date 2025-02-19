c4ec2e7ec21e97691a8a4054cd9007cb8e339164
= <=>
2025-02-19 07:57:51 -0300

    test: implement list genre IT tests

---
+++ infrastructure/src/test/java/com/codeflix/admin/catalog/application/genre/retrieve/list/ListGenreUseCaseIT.java
@@ -0,0 +1,85 @@
+package com.codeflix.admin.catalog.application.genre.retrieve.list;
+
+import com.codeflix.admin.catalog.IntegrationTest;
+import com.codeflix.admin.catalog.domain.genre.Genre;
+import com.codeflix.admin.catalog.domain.genre.GenreGateway;
+import com.codeflix.admin.catalog.domain.pagination.SearchQuery;
+import com.codeflix.admin.catalog.infrastructure.genre.persistence.GenreJpaEntity;
+import com.codeflix.admin.catalog.infrastructure.genre.persistence.GenreRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+
+@IntegrationTest
+public class ListGenreUseCaseIT {
+    @Autowired
+    private DefaultListGenreUseCase useCase;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Test
+    public void givenAValidQuery_whenCallsListGenre_shouldReturnGenres() {
+        final var genres = List.of(
+                Genre.newGenre("Action", true),
+                Genre.newGenre("Adventure", true)
+        );
+
+        genreRepository.saveAllAndFlush(genres.stream().map(GenreJpaEntity::from).toList());
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 2;
+
+        final var expectedItems = genres.stream()
+                .map(GenreListOutput::from)
+                .toList();
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var actualOutput = useCase.execute(aQuery);
+
+        Assertions.assertEquals(expectedPage, actualOutput.currentPage());
+        Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
+        Assertions.assertEquals(expectedTotal, actualOutput.total());
+        Assertions.assertTrue(
+                expectedItems.size() == actualOutput.items().size()
+                        && expectedItems.containsAll(actualOutput.items())
+        );
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListGenreAndResultIsEmpty_shouldReturnGenres() {
+        final var expectedPage = 0;
+        final var expectedPerPage = 10;
+        final var expectedTerms = "A";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+        final var expectedTotal = 0;
+
+        final var expectedItems = List.<GenreListOutput>of();
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var actualOutput = useCase.execute(aQuery);
+
+        Assertions.assertEquals(expectedPage, actualOutput.currentPage());
+        Assertions.assertEquals(expectedPerPage, actualOutput.perPage());
+        Assertions.assertEquals(expectedTotal, actualOutput.total());
+        Assertions.assertEquals(expectedItems, actualOutput.items());
+    }
+}
