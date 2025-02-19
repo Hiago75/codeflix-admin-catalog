e8552ddfeb88e7a0f2a1b1220dd3c2d741e187e8
Hiago <hiagobrenha@protonmail.com>
2025-02-19 07:31:35 -0300

    test: implement get genre IT tests

---
+++ infrastructure/src/test/java/com/codeflix/admin/catalog/application/genre/retrieve/get/GetGenreByIdUseCaseIT.java
@@ -0,0 +1,78 @@
+package com.codeflix.admin.catalog.application.genre.retrieve.get;
+
+import com.codeflix.admin.catalog.IntegrationTest;
+import com.codeflix.admin.catalog.domain.category.Category;
+import com.codeflix.admin.catalog.domain.category.CategoryGateway;
+import com.codeflix.admin.catalog.domain.category.CategoryID;
+import com.codeflix.admin.catalog.domain.exceptions.NotFoundException;
+import com.codeflix.admin.catalog.domain.genre.Genre;
+import com.codeflix.admin.catalog.domain.genre.GenreGateway;
+import com.codeflix.admin.catalog.domain.genre.GenreID;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@IntegrationTest
+public class GetGenreByIdUseCaseIT {
+    @Autowired
+    private DefaultGetGenreByIdUseCase useCase;
+
+    @Autowired
+    private GenreGateway genreGateway;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidId_whenCallsGetGenre_shouldReturnGenre() {
+        final var movies = categoryGateway.create(Category.newCategory("Movies", null, true));
+        final var series = categoryGateway.create(Category.newCategory("Series", null, true));
+
+        final var expectedName = "Action";
+        final var expectedIsActive = true;
+        final var expectedCategories = List.of(
+                movies.getId(),
+                series.getId()
+        );
+
+        final var aGenre = genreGateway.create(Genre.newGenre(expectedName, expectedIsActive)
+                .addCategories(expectedCategories));
+        final var expectedId = aGenre.getId();
+
+        final var actualGenre = useCase.execute(expectedId.getValue());
+
+        assertEquals(expectedId.getValue(), actualGenre.id());
+        assertEquals(expectedName, actualGenre.name());
+        assertEquals(expectedIsActive, actualGenre.isActive());
+        assertTrue(
+        expectedCategories.size() == actualGenre.categories().size()
+                && asString(expectedCategories).containsAll(actualGenre.categories())
+        );
+        assertEquals(aGenre.getCreatedAt(), actualGenre.createdAt());
+        assertEquals(aGenre.getUpdatedAt(), actualGenre.updatedAt());
+        assertEquals(aGenre.getDeletedAt(), actualGenre.deletedAt());
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetGenreAndDoesNotExists_shouldReturnNotFound() {
+        final var expectedErrorMessage = "Genre with ID 123 was not found";
+        final var expectedId = GenreID.from("123");
+
+        final var actualException = assertThrows(NotFoundException.class, () -> useCase.execute(expectedId.getValue()));
+
+        assertEquals(expectedErrorMessage, actualException.getMessage());
+    }
+
+    private List<String> asString(final List<CategoryID> categories) {
+        return categories.stream()
+                .map(CategoryID::getValue)
+                .toList();
+    }
+}
