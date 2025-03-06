90425a505ea346edd9eb819971e904509e032c20
Hiago <hiagobrenha@protonmail.com>
2025-03-06 08:14:22 -0300

    feat: implement list cast member gateway

---
+++ application/src/main/java/com/codeflix/admin/catalog/application/castmember/retrieve/list/DefaultListCastMembersUseCase.java
@@ -0,0 +1,21 @@
+package com.codeflix.admin.catalog.application.castmember.retrieve.list;
+
+import com.codeflix.admin.catalog.domain.castmember.CastMemberGateway;
+import com.codeflix.admin.catalog.domain.pagination.Pagination;
+import com.codeflix.admin.catalog.domain.pagination.SearchQuery;
+
+import java.util.Objects;
+
+public final class DefaultListCastMembersUseCase extends ListCastMembersUseCase{
+    private CastMemberGateway castMemberGateway;
+
+    public DefaultListCastMembersUseCase(final CastMemberGateway castMemberGateway) {
+        this.castMemberGateway = Objects.requireNonNull(castMemberGateway);
+    }
+
+    @Override
+    public Pagination<CastMemberListOutput> execute(SearchQuery aQuery) {
+        return this.castMemberGateway.findAll(aQuery)
+                .map(CastMemberListOutput::from);
+    }
+}
