62ffed7863bef6243891627c82f3eebe86cc8a80
Hiago <hiagobrenha@protonmail.com>
2025-03-22 14:18:35 -0300

    test: add create cast member use case IT

---
+++ infrastructure/src/test/java/com/codeflix/admin/catalog/application/castmember/create/CreateCastMemberUseCaseIT.java
@@ -0,0 +1,91 @@
+package com.codeflix.admin.catalog.application.castmember.create;
+
+import com.codeflix.admin.catalog.Fixture;
+import com.codeflix.admin.catalog.IntegrationTest;
+import com.codeflix.admin.catalog.domain.castmember.CastMemberGateway;
+import com.codeflix.admin.catalog.domain.castmember.CastMemberType;
+import com.codeflix.admin.catalog.domain.exceptions.NotificationException;
+import com.codeflix.admin.catalog.infrastructure.castmember.persistence.CastMemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@IntegrationTest
+public class CreateCastMemberUseCaseIT {
+    @Autowired
+    private CreateCastMemberUseCase useCase;
+
+    @Autowired
+    private CastMemberRepository castMemberRepository;
+
+    @SpyBean
+    private CastMemberGateway castMemberGateway;
+
+    @Test
+    public void givenAValidCommand_whenCallsCreateCastMember_shouldReturnIt() {
+        final var expectedName = Fixture.name();
+        final var expectedType = Fixture.CastMember.type();
+
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
+
+        final var actualOutput = useCase.execute(aCommand);
+        assertNotNull(actualOutput);
+        assertNotNull(actualOutput.id());
+
+        final var actualMember = this.castMemberRepository.findById(actualOutput.id()).get();
+
+        assertEquals(expectedName, actualMember.getName());
+        assertEquals(expectedType, actualMember.getType());
+        assertNotNull(actualMember.getCreatedAt());
+        assertNotNull(actualMember.getUpdatedAt());
+        assertEquals(actualMember.getCreatedAt(), actualMember.getUpdatedAt());
+
+        verify(castMemberGateway, times(1)).create(any());
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallsCreateCastMember_shouldThrowsNotificationException() {
+        final String expectedName = null;
+        final var expectedType = Fixture.CastMember.type();
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
+
+        final var actualException = assertThrows(NotificationException.class, () -> {
+            useCase.execute(aCommand);
+        });
+
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(castMemberGateway, times(0)).create(any());
+    }
+
+    @Test
+    public void givenAInvalidType_whenCallsCreateCastMember_shouldThrowsNotificationException() {
+        final var expectedName = Fixture.name();
+        final CastMemberType expectedType = null;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'type' should not be null";
+
+        final var aCommand = CreateCastMemberCommand.with(expectedName, expectedType);
+
+        final var actualException = assertThrows(NotificationException.class, () -> {
+            useCase.execute(aCommand);
+        });
+
+        assertNotNull(actualException);
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(castMemberGateway, times(0)).create(any());
+    }
+}
