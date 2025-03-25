23b0eb7f0148ed30a37ea7c270d492499c1afff6
Hiago <hiagobrenha@protonmail.com>
2025-03-24 21:58:17 -0300

    feat: implement video update

--- domain/src/main/java/com/codeflix/admin/catalog/domain/video/Video.java
+++ domain/src/main/java/com/codeflix/admin/catalog/domain/video/Video.java
@@ -143,12 +143,71 @@ public class Video extends AggregateRoot<VideoID> {
         );
     }
 
-    public String getTitle() {
-        return title;
+    public Video update(
+            final String aTitle,
+            final String aDescription,
+            final Year aLaunchedAt,
+            final double aDuration,
+            final Rating aRating,
+            final boolean isOpened,
+            final boolean isPublished,
+            final Set<CategoryID> aCategories,
+            final Set<GenreID> aGenres,
+            final Set<CastMemberID> aCastMembers
+    ) {
+        this.title = aTitle;
+        this.description = aDescription;
+        this.launchedAt = aLaunchedAt;
+        this.duration = aDuration;
+        this.rating = aRating;
+        this.opened = isOpened;
+        this.published = isPublished;
+        this.setCategories(aCategories);
+        this.setGenres(aGenres);
+        this.setCastMembers(aCastMembers);
+
+        this.updatedAt = InstantUtils.now();
+
+        return this;
     }
 
-    public void setTitle(final String title) {
-        this.title = title;
+    public Video setBanner(final ImageMedia banner) {
+        this.banner = banner;
+        this.updatedAt = InstantUtils.now();
+
+        return this;
+    }
+
+    public Video setThumbnail(final ImageMedia thumbnail) {
+        this.thumbnail = thumbnail;
+        this.updatedAt = InstantUtils.now();
+
+        return this;
+    }
+
+    public Video setThumbnailHalf(final ImageMedia thumbnailHalf) {
+        this.thumbnailHalf = thumbnailHalf;
+        this.updatedAt = InstantUtils.now();
+
+        return this;
+    }
+
+    public Video setTrailer(final AudioVideoMedia trailer) {
+        this.trailer = trailer;
+        this.updatedAt = InstantUtils.now();
+
+        return this;
+    }
+
+    public Video setVideo(final AudioVideoMedia video) {
+        this.video = video;
+        this.updatedAt = InstantUtils.now();
+
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public String getDescription() {
@@ -163,119 +222,71 @@ public class Video extends AggregateRoot<VideoID> {
         return launchedAt;
     }
 
-    public void setLaunchedAt(final Year launchedAt) {
-        this.launchedAt = launchedAt;
-    }
-
     public double getDuration() {
         return duration;
-    }
-
-    public void setDuration(final double duration) {
-        this.duration = duration;
     }
 
     public Rating getRating() {
         return rating;
     }
 
-    public void setRating(final Rating rating) {
-        this.rating = rating;
-    }
-
     public boolean isOpened() {
         return opened;
-    }
-
-    public void setOpened(final boolean opened) {
-        this.opened = opened;
     }
 
     public boolean isPublished() {
         return published;
     }
 
-    public void setPublished(final boolean published) {
-        this.published = published;
-    }
-
     public Instant getCreatedAt() {
         return createdAt;
-    }
-
-    public void setCreatedAt(final Instant createdAt) {
-        this.createdAt = createdAt;
     }
 
     public Instant getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(final Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
     public Optional<ImageMedia> getBanner() {
         return Optional.ofNullable(banner);
-    }
-
-    public void setBanner(final ImageMedia banner) {
-        this.banner = banner;
     }
 
     public Optional<ImageMedia> getThumbnail() {
         return Optional.ofNullable(thumbnail);
     }
 
-    public void setThumbnail(final ImageMedia thumbnail) {
-        this.thumbnail = thumbnail;
-    }
-
     public Optional<ImageMedia> getThumbnailHalf() {
         return Optional.ofNullable(thumbnailHalf);
-    }
-
-    public void setThumbnailHalf(final ImageMedia thumbnailHalf) {
-        this.thumbnailHalf = thumbnailHalf;
     }
 
     public Optional<AudioVideoMedia> getTrailer() {
         return Optional.ofNullable(trailer);
     }
 
-    public void setTrailer(final AudioVideoMedia trailer) {
-        this.trailer = trailer;
-    }
-
     public Optional<AudioVideoMedia> getVideo() {
         return Optional.ofNullable(video);
-    }
-
-    public void setVideo(final AudioVideoMedia video) {
-        this.video = video;
     }
 
     public Set<CategoryID> getCategories() {
         return categories != null ? Collections.unmodifiableSet(categories) : Collections.emptySet();
     }
 
-    public void setCategories(final Set<CategoryID> categories) {
-        this.categories = categories != null ? new HashSet<>(categories) : Collections.emptySet();
+    public Set<CastMemberID> getCastMembers() {
+        return castMembers != null ? Collections.unmodifiableSet(castMembers) : Collections.emptySet();
     }
 
     public Set<GenreID> getGenres() {
         return genres != null ? Collections.unmodifiableSet(genres) : Collections.emptySet();
     }
 
-    public void setGenres(final Set<GenreID> genres) {
+    private void setCategories(final Set<CategoryID> categories) {
+        this.categories = categories != null ? new HashSet<>(categories) : Collections.emptySet();
+    }
+
+    private void setGenres(final Set<GenreID> genres) {
         this.genres = genres != null ? new HashSet<>(genres) : Collections.emptySet();
     }
 
-    public Set<CastMemberID> getCastMembers() {
-        return castMembers != null ? Collections.unmodifiableSet(castMembers) : Collections.emptySet();
-    }
-
-    public void setCastMembers(final Set<CastMemberID> castMembers) {
+    private void setCastMembers(final Set<CastMemberID> castMembers) {
         this.castMembers = castMembers != null ? new HashSet<>(castMembers) : Collections.emptySet();
     }
 }
--- domain/src/test/java/com/codeflix/admin/catalog/domain/video/VideoTest.java
+++ domain/src/test/java/com/codeflix/admin/catalog/domain/video/VideoTest.java
@@ -45,6 +45,9 @@ public class VideoTest {
 
         assertNotNull(actualVideo);
         assertNotNull(actualVideo.getId());
+        assertNotNull(actualVideo.getCreatedAt());
+        assertNotNull(actualVideo.getUpdatedAt());
+        assertEquals(actualVideo.getCreatedAt(), actualVideo.getUpdatedAt());
         assertEquals(expectedTitle, actualVideo.getTitle());
         assertEquals(expectedDescription, actualVideo.getDescription());
         assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
@@ -60,6 +63,345 @@ public class VideoTest {
         assertTrue(actualVideo.getBanner().isEmpty());
         assertTrue(actualVideo.getThumbnail().isEmpty());
         assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsUpdate_shouldReturnUpdated() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                "Test title",
+                "Test desc",
+                Year.of(1900),
+                0.0,
+                Rating.AGE_10,
+                true,
+                true,
+                Set.of(),
+                Set.of(),
+                Set.of()
+        );
+
+        final var actualVideo = Video.with(aVideo).update(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertNotNull(actualVideo.getCreatedAt());
+        assertNotNull(actualVideo.getUpdatedAt());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration(), 0.01);
+        assertEquals(expectedOpened, actualVideo.isOpened());
+        assertEquals(expectedPublished, actualVideo.isPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetVideo_shouldReturnUpdated() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aVideoMedia = AudioVideoMedia.with("avc", "video.mp4", "/123/videos", "", MediaStatus.PENDING);
+
+        final var actualVideo = Video.with(aVideo).setVideo(aVideoMedia);
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertNotNull(actualVideo.getCreatedAt());
+        assertNotNull(actualVideo.getUpdatedAt());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration(), 0.01);
+        assertEquals(expectedOpened, actualVideo.isOpened());
+        assertEquals(expectedPublished, actualVideo.isPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertEquals(aVideoMedia, actualVideo.getVideo().get());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetTrailer_shouldReturnUpdated() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aTrailerMedia = AudioVideoMedia.with("avc", "trailer.mp4", "/123/videos", "", MediaStatus.PENDING);
+
+        final var actualVideo = Video.with(aVideo).setTrailer(aTrailerMedia);
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertNotNull(actualVideo.getCreatedAt());
+        assertNotNull(actualVideo.getUpdatedAt());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration(), 0.01);
+        assertEquals(expectedOpened, actualVideo.isOpened());
+        assertEquals(expectedPublished, actualVideo.isPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertEquals(aTrailerMedia, actualVideo.getTrailer().get());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetBanner_shouldReturnUpdated() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aBannerMedia = ImageMedia.with("avc", "trailer.mp4", "/123/videos");
+
+        final var actualVideo = Video.with(aVideo).setBanner(aBannerMedia);
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertNotNull(actualVideo.getCreatedAt());
+        assertNotNull(actualVideo.getUpdatedAt());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration(), 0.01);
+        assertEquals(expectedOpened, actualVideo.isOpened());
+        assertEquals(expectedPublished, actualVideo.isPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertEquals(aBannerMedia, actualVideo.getBanner().get());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetThumbnail_shouldReturnUpdated() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aThumbMedia = ImageMedia.with("avc", "trailer.mp4", "/123/videos");
+
+        final var actualVideo = Video.with(aVideo).setThumbnail(aThumbMedia);
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertNotNull(actualVideo.getCreatedAt());
+        assertNotNull(actualVideo.getUpdatedAt());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration(), 0.01);
+        assertEquals(expectedOpened, actualVideo.isOpened());
+        assertEquals(expectedPublished, actualVideo.isPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertEquals(aThumbMedia, actualVideo.getThumbnail().get());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
+        assertTrue(actualVideo.getThumbnailHalf().isEmpty());
+
+        assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenValidVideo_whenCallsSetThumbnailHalf_shouldReturnUpdated() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers = Set.of(CastMemberID.unique());
+
+        final var aVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var aThumbnailHalfMedia = ImageMedia.with("avc", "trailer.mp4", "/123/videos");
+
+        final var actualVideo = Video.with(aVideo).setThumbnailHalf(aThumbnailHalfMedia);
+
+        assertNotNull(actualVideo);
+        assertNotNull(actualVideo.getId());
+        assertNotNull(actualVideo.getCreatedAt());
+        assertNotNull(actualVideo.getUpdatedAt());
+        assertEquals(aVideo.getCreatedAt(), actualVideo.getCreatedAt());
+        assertTrue(aVideo.getUpdatedAt().isBefore(actualVideo.getUpdatedAt()));
+        assertEquals(expectedTitle, actualVideo.getTitle());
+        assertEquals(expectedDescription, actualVideo.getDescription());
+        assertEquals(expectedLaunchedAt, actualVideo.getLaunchedAt());
+        assertEquals(expectedDuration, actualVideo.getDuration(), 0.01);
+        assertEquals(expectedOpened, actualVideo.isOpened());
+        assertEquals(expectedPublished, actualVideo.isPublished());
+        assertEquals(expectedRating, actualVideo.getRating());
+        assertEquals(expectedCategories, actualVideo.getCategories());
+        assertEquals(expectedGenres, actualVideo.getGenres());
+        assertEquals(expectedMembers, actualVideo.getCastMembers());
+        assertEquals(aThumbnailHalfMedia, actualVideo.getThumbnailHalf().get());
+        assertTrue(actualVideo.getVideo().isEmpty());
+        assertTrue(actualVideo.getTrailer().isEmpty());
+        assertTrue(actualVideo.getThumbnail().isEmpty());
+        assertTrue(actualVideo.getBanner().isEmpty());
 
         assertDoesNotThrow(() -> actualVideo.validate(new ThrowsValidationHandler()));
     }
