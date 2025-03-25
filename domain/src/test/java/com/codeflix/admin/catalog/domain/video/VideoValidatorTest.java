43829a9cc1b561c503d2fa4210f0d99862fcf846
Hiago <hiagobrenha@protonmail.com>
2025-03-24 21:37:19 -0300

    feat: implement video validator

---
+++ domain/src/test/java/com/codeflix/admin/catalog/domain/video/VideoValidatorTest.java
@@ -0,0 +1,274 @@
+package com.codeflix.admin.catalog.domain.video;
+
+import com.codeflix.admin.catalog.domain.castmember.CastMemberID;
+import com.codeflix.admin.catalog.domain.category.CategoryID;
+import com.codeflix.admin.catalog.domain.exceptions.DomainException;
+import com.codeflix.admin.catalog.domain.genre.GenreID;
+import com.codeflix.admin.catalog.domain.validation.handler.ThrowsValidationHandler;
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VideoValidatorTest {
+    @Test
+    public void givenNullTitle_whenCallsValidate_shouldReceiveError() {
+        final String expectedTitle = null;
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'title' should not be null";
+
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        final var actualError = assertThrows(DomainException.class, validator::validate);
+
+        assertEquals(expectedErrorCount, actualError.getErrors().size());
+        assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenEmptyTitle_whenCallsValidate_shouldReceiveError() {
+        final String expectedTitle = "";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'title' should not be empty";
+
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        final var actualError = assertThrows(DomainException.class, validator::validate);
+
+        assertEquals(expectedErrorCount, actualError.getErrors().size());
+        assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenTitleWithLengthGreaterThan255_whenCallsValidate_shouldReceiveError() {
+        final String expectedTitle = "a".repeat(256);
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'title' must be between 1 and 255 characters";
+
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        final var actualError = assertThrows(DomainException.class, validator::validate);
+
+        assertEquals(expectedErrorCount, actualError.getErrors().size());
+        assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenEmptyDescription_whenCallsValidate_shouldReceiveError() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "";
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'description' should not be empty";
+
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        final var actualError = assertThrows(DomainException.class, validator::validate);
+
+        assertEquals(expectedErrorCount, actualError.getErrors().size());
+        assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenDescriptionWithLengthGreaterThan4000_whenCallsValidate_shouldReceiveError() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Ea".repeat(4001);
+        final var expectedLaunchedAt = Year.of(2022);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'description' must be between 1 and 4000 characters";
+
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        final var actualError = assertThrows(DomainException.class, validator::validate);
+
+        assertEquals(expectedErrorCount, actualError.getErrors().size());
+        assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenNullLaunchedAt_whenCallsValidate_shouldReceiveError() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final Year expectedLaunchedAt = null;
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final var expectedRating = Rating.L;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'launchedAt' should not be null";
+
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        final var actualError = assertThrows(DomainException.class, validator::validate);
+
+        assertEquals(expectedErrorCount, actualError.getErrors().size());
+        assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenNullRating_whenCallsValidate_shouldReceiveError() {
+        final var expectedTitle = "System design";
+        final var expectedDescription = "Estudo de caso";
+        final var expectedLaunchedAt = Year.of(2025);
+        final var expectedDuration = 120.10;
+        final var expectedOpened = false;
+        final var expectedPublished = false;
+        final Rating expectedRating = null;
+        final var expectedCategories = Set.of(CategoryID.unique());
+        final var expectedGenres = Set.of(GenreID.unique());
+        final var expectedMembers =  Set.of(CastMemberID.unique());
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'rating' should not be null";
+
+        final var actualVideo = Video.newVideo(
+                expectedTitle,
+                expectedDescription,
+                expectedLaunchedAt,
+                expectedDuration,
+                expectedRating,
+                expectedOpened,
+                expectedPublished,
+                expectedCategories,
+                expectedGenres,
+                expectedMembers
+        );
+
+        final var validator = new VideoValidator(actualVideo, new ThrowsValidationHandler());
+
+        final var actualError = assertThrows(DomainException.class, validator::validate);
+
+        assertEquals(expectedErrorCount, actualError.getErrors().size());
+        assertEquals(expectedErrorMessage, actualError.getErrors().get(0).message());
+    }
+}
