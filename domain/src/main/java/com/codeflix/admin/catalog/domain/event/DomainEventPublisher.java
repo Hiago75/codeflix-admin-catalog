5f2f37de0986d5bb6172135d0a6ce96039ff0026
= <hiagobrenha@protonmail.com>
2025-04-23 19:02:21 -0300

    tests: add tests to entity

--- domain/src/main/java/com/codeflix/admin/catalog/domain/event/DomainEventPublisher.java
+++ domain/src/main/java/com/codeflix/admin/catalog/domain/event/DomainEventPublisher.java
@@ -2,5 +2,5 @@ package com.codeflix.admin.catalog.domain.event;
 
 @FunctionalInterface
 public interface DomainEventPublisher {
-    <T extends DomainEvent> void publishEvent(final T event);
+    void publishEvent(DomainEvent event);
 }
