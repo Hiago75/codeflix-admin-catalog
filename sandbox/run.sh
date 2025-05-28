3d17187e673c4c5d1b89fbe063a02adb278233c1
= <hiagobrenha@protonmail.com>
2025-05-28 18:29:50 -0300

    chore: add elasticsearch

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,10 +1,14 @@
 docker network create catalog_adm_services
-docker network create catalog_adm_network
+docker network create elastic
 
+sudo chown root app/filebeat/filebeat.docker.yml
 mkdir -m 777 .docker
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/es01
+mkdir -m 777 .docker/filebeat
 
 docker-compose -f services/docker-compose.yml up -d
+docker-compose -f elk/docker-compose.yml up -d
 
 echo "Starting containers..."
 sleep 5
